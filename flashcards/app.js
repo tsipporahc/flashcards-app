3ea9cc8fd4314389895ce7855cc53b775615cc93
tsipporahc <tsipporahc@gmail.com>
2022-10-06 22:42:47 -0700

    Add pug file to site

--- flashcards/app.js
+++ flashcards/app.js
@@ -2,10 +2,20 @@ const express = require('express');
 
 const app = express(); // returns express app
 
-app.get('/', (request, response) => {
-    response.send('I love you');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.send('<h1>I love you<h1>');
+
+}); // root route
+
+app.get('/hello', (req, res) => {
+    res.send('<h1>Hello YOU!!<h1>');
 
 });
 
+
 //set up developer server using the listen method
-app.listen(3000); // 3000 is port number
+app.listen(3000, () => {
+    console.log('The app running on localhost:3000');
+}); // 3000 is port number
