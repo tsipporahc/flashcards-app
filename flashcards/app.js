e00844a732e7515267ee8ca91cc793cc6c026a16
tsipporahc <tsipporahc@gmail.com>
2022-10-06 21:49:49 -0700

    Set up get requeston the app object

--- flashcards/app.js
+++ flashcards/app.js
@@ -1,1 +1,11 @@
 const express = require('express');
+
+const app = express(); // returns express app
+
+app.get('/', (request, response) => {
+    response.send('I love you');
+
+});
+
+//set up developer server using the listen method
+app.listen(3000); // 3000 is port number
