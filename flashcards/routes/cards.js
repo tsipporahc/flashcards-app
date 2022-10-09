2abb1292e4c6ed8f71b9e6f9e5dd53cebc2d1a1f
tsipporahc <tsipporahc@gmail.com>
2022-10-08 21:50:11 -0700

    Add Links to flip card and create query string variables

--- flashcards/routes/cards.js
+++ flashcards/routes/cards.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../data/flashcardData.json').data;
+const { cards } = data;
 
-router.get('/', (req, res) => {
-    res.render('cards', {prompt: 'Who love you?', hint: 'the person that created this router'});
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const { side } = req.query; // question or answer side of card
+    const text = cards[id][side];
+    const { hint } = cards[id];
+    const templateData = { id, side, text };
+
+    if (side === 'question') {
+        templateData.hint = hint;
+        templateData.side = 'answer';
+        templateData.sideText = 'Answer';
+    } else if (side === 'answer') {
+        templateData.side = 'question';
+        templateData.sideText = 'Question';
+    }
+
+    res.render('cards', templateData);
 
 }); //cards route
 
